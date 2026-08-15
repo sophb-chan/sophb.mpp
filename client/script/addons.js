b342cad24d1af41f2cadbf957a093a32151d8e60
Sophia Blair <sophb.ccjt@proton.me>
2026-08-15 14:25:48 -0300

    fix reddit button and fix style override in top buttons

--- client/script/addons.js
+++ client/script/addons.js
@@ -1,21 +1,21 @@
 const userscriptURLs = [
-    'https://greasyfork.org/en/scripts/554578-tealmidiplayer',
-    'https://greasyfork.org/en/scripts/582106-token-switcher',
-    'https://greasyfork.org/en/scripts/567148-eval',
-    'https://greasyfork.org/en/scripts/542677-multiplayer-piano-optimizations-emotes'
+	'https://greasyfork.org/en/scripts/554578-tealmidiplayer',
+	'https://greasyfork.org/en/scripts/582106-token-switcher',
+	'https://greasyfork.org/en/scripts/567148-eval',
+	'https://greasyfork.org/en/scripts/542677-multiplayer-piano-optimizations-emotes'
 ];
 for (const userscriptURL of userscriptURLs) {
-    let attempts = 0;
-    while (true) {
-        try {
-            runFromGreasyfork(userscriptURL);
-            break;
-        } catch (err) {
-            attempts++
-            if (attempts === 15) {
-                console.error('Failed to load an addon.');
-                break;
-            };
-        }
-    }
+	let attempts = 0;
+	while (true) {
+		try {
+			runFromGreasyfork(userscriptURL);
+			break;
+		} catch (err) {
+			attempts++
+			if (attempts === 15) {
+				console.error('Failed to load an addon.');
+				break;
+			};
+		}
+	}
 }
