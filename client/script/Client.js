d64b46a4a0484ad3d17f6a7f273eff9ee0a728db
Sophia Blair <sophb.ccjt@proton.me>
2026-08-26 18:31:46 -0300

    remove unecessary IIFE

--- client/script/Client.js
+++ client/script/Client.js
@@ -8,8 +8,8 @@ WebSocket.prototype.send = new Proxy(WebSocket.prototype.send, {
 
 class Client extends EventEmitter {
 	constructor(uri) {
-		if (window.MPP && MPP.client)
-			console.warn(
+		if (MPP?.client != null)
+			throw new Error(
 				"Running multiple clients in a single tab is not allowed due to abuse. Attempting to bypass this may result in an auto-ban!",
 			);
 
