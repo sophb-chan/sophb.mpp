36e0f3af2eb7e166d88ad4cf3c544a858eeac77e
Sophia Blair <sophb.ccjt@proton.me>
2026-09-05 23:33:03 -0300

    Fix collision between named and plain link

--- client/script/util.js
+++ client/script/util.js
@@ -284,13 +284,13 @@ const URLregexPattern =
 // resource path (optional)
 	"(?:[/?#]\\S*)?";
 const markdownPatterns = {
+	link: {
+		regex: new RegExp(`((?<!\\[.+?\]\\()${URLregexPattern})`, "ig"),
+		replacer: '<a rel="noreferer noopener" target="_blank" class="chatLink" href="$1">$1</a>',
+	},
 	namedLink: {
 		regex: new RegExp(`\\[(.+?)\]\\((${URLregexPattern})\\)`, 'gi'),
 		replacer: '<a rel="noreferer noopener" target="_blank" class="chatLink" href="$2">$1</a>',
-	},
-	link: {
-		regex: new RegExp(`(${URLregexPattern})`, "ig"),
-		replacer: '<a rel="noreferer noopener" target="_blank" class="chatLink" href="$1">$1</a>',
 	},
 	strikethrough: {
 		regex: /~~(.+?)~~/ig,
