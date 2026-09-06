c6bf305fdb0982c305fb3d8ee5ca71e9d4666b99
Sophia Blair <sophb.ccjt@proton.me>
2026-09-06 00:04:46 -0300

    Add default language to i18n

--- client/script/script.js
+++ client/script/script.js
@@ -1,7 +1,8 @@
 const translation = window.i18nextify.init({
+	lng: 'en',
+	fallbackLng: 'en',
 	autorun: false,
 });
-protoplus.expand();
 
 if (location.host === "multiplayerpiano.com") {
 	const url = new URL("https://multiplayerpiano.net/" + location.search);
