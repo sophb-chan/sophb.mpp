aea7e820bdee99f9830daf7ca209274e2d9357aa
Sophia Blair <sophb.ccjt@proton.me>
2026-09-16 12:45:00 -0300

    Remove notification about non-userscript environments

--- client/script/addons/tealmidiplayer.js
+++ client/script/addons/tealmidiplayer.js
@@ -1964,7 +1964,7 @@ async function checkUpdate(isInterval, silent = false) {
 							notifMsg = `You are using an unreleased version of <b>${name}</b>.\nExpect instability and bugs.`;
 						break;
 					case 'unknown': case 'devtools':
-						if (!isInterval)
+						if (!isInterval && false)
 							notifMsg = `You are using ${name} on a non-userscript environment.\nSome features may be broken.`;
 						break;
 					default: notifMsg = null; break;
